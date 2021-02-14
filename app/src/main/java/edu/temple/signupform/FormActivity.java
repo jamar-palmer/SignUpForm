911aea1530882d04eaf748849ea9662d72f2afbf
Jamar <64226908+jamar-palmer@users.noreply.github.com>
2021-02-14 15:30:47 -0500

    Completed

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -42,39 +42,50 @@ public class FormActivity extends AppCompatActivity {
         errPass = findViewById(R.id.passError);
         errPass2 = findViewById(R.id.passError2);
 
-        //anonymous class
+        //anonymous class compares for empty string or whitespaces
         View.OnClickListener display = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(txtName.getText().toString() == null){
+                if(txtName.getText().toString().equals("") || txtName.getText().toString().trim().equals("")){
+                    hide();
                     errName.setVisibility(View.VISIBLE);
                     errName.setText("Please Enter Your Name");
-                }else if(txtEmail.getText().toString() == null){
+                }else if(txtEmail.getText().toString().equals("") || txtEmail.getText().toString().trim().equals("")){
+                    hide();
                     errEmail.setVisibility(View.VISIBLE);
                     errEmail.setText("Please Enter Your Email");
-                }else if(txtPassword.getText().toString() == null){
+                }else if(txtPassword.getText().toString().equals("") || txtPassword.getText().toString().trim().equals("")){
+                    hide();
                     errPass.setVisibility(View.VISIBLE);
                     errPass.setText("Please Enter Your Password");
-                }else if(txtRename.getText().toString() == null){
+                }else if(txtRename.getText().toString().equals("") || txtRename.getText().toString().trim().equals("")){
+                    hide();
                     errPass2.setVisibility(View.VISIBLE);
                     errPass2.setText("Please Re-Enter Your Password");
-                }else if(txtPassword.getText().toString().equals(txtRename.getText().toString())){
+                }else if(!txtPassword.getText().toString().equals(txtRename.getText().toString())){
+                    hide();
                     errPass2.setVisibility(View.VISIBLE);
                     errPass2.setText("Passwords do not match");
                 }
                 else{
-                    errName.setVisibility(View.INVISIBLE);
-                    errEmail.setVisibility(View.INVISIBLE);
-                    errPass.setVisibility(View.INVISIBLE);
-                    errPass2.setVisibility(View.INVISIBLE);
+                    hide();
                     Toast.makeText(FormActivity.this, "Welcome, " + txtName.getText().toString() + ", to the SignUpForm App", Toast.LENGTH_SHORT).show();
                 }
+
+                //method for hiding error messages
             }
         };
 
         //defining onClick event
         save.setOnClickListener(display);
 
+    }
+
+    public void hide(){
+        errName.setVisibility(View.INVISIBLE);
+        errEmail.setVisibility(View.INVISIBLE);
+        errPass.setVisibility(View.INVISIBLE);
+        errPass2.setVisibility(View.INVISIBLE);
     }
 
 }
